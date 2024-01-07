a8e90155c4890243260e1f7e1e0311582cc0c35e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-06 23:22:21 -0300

    create cart with the user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,15 +10,15 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    render json: [@user, @user.cart]
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
-
+    @user.build_cart 
+  
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created, location: api_v1_user_url(@user)
     else
       render json: @user.errors, status: :unprocessable_entity
     end
