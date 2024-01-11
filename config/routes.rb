75c6976ffc0ea37d2ead4e15d498ecdc7a4573f6
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-11 01:01:16 -0300

    add users show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # resources :cart_items
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:show]
       resources :categories, only: [:index,  :show]
       resources :items do
         collection do
