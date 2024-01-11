2808af4c429731facdc66c3c4419cbd3f21d9d99
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-11 01:00:44 -0300

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Cart.destroy_all
 User.destroy_all
 
 Item.destroy_all
@@ -23,7 +24,6 @@ end
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.sentence,
     price: Faker::Commerce.price(range: 50.0..1000.0),
-    image: 'not available',
     quantity: rand(1..10),
     category: Category.all.sample,
     image: 'https://images.unsplash.com/photo-1624701928517-44c8ac49d93c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
