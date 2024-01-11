38bf7e9e905ef7e03529bd3c47ac3ed927f222e9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-10 23:55:56 -0300

    fixed typos

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
-  belongs_to :User
+  belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
 
-  validates :total_price, presence: :true
+  validates :total_price, presence: true
 end
