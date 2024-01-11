f78f3831448001a6fd8eca14fdff0f3caa3d4f4c
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-01-10 23:56:15 -0300

    add after_create to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_one :cart
 
+  after_create :create_cart
+
   validates :name, presence: :true
   validates :email, presence: :true
   validates :password, presence: :true
+
+  private
+  
+  def create_cart
+    Cart.create(user: self)
+  end
 end
