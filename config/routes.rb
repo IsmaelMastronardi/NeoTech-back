864eacb60f572e88dba69e9de2663ff470c34277
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-03 00:40:56 -0300

    Add nested resources for orders in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users , only: [:show] do
+      resources :users, only: [:show] do
         collection do
           post :create_temporary_user
         end
+        resources :orders, only: [:show] do
+          collection do
+            post :fill_and_complete_order
+          end
+        end
       end
-      resources :categories, only: [:index,  :show]
+      resources :categories, only: [:index, :show]
       resources :items do
         collection do
           get :recentlyAdded
         end
-      end
-      resources :users, only: [:show, :create, :delete] do
-        resources :carts
       end
     end
   end
