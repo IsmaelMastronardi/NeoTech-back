2e94f2146e3c27455017065a076726a89d499def
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-02-26 23:42:17 -0300

    Update Gemfile.lock and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
       resources :categories, only: [:index, :show]
       resources :items do
         collection do
-          get :recently_Added
+          get :recently_added
         end
       end
     end
